0c1f5d0ff9b87f1306b69ff33a24abb198a707f7
Msicuro <35858231+Msicuro@users.noreply.github.com>
2021-03-10 23:21:56 -0500

    Fixed deselect joints script

--- deselect_end_jnts.py
+++ deselect_end_jnts.py
@@ -4,6 +4,7 @@ class deselectEndJointsClass(object):
 	"""docstring for deselectEndJointsClass"""
 	def __init__(self, *args):
 		self.variables = {}
+		self.createUI()
 		
 	#Create the UI window
 	def createUI(self, *args):
@@ -11,14 +12,13 @@ class deselectEndJointsClass(object):
 		self.variables["rowLayout"] = cmds.rowColumnLayout(numberOfRows = 1)
 		self.variables["layout_text"] = cmds.text(label='Selection')
 		self.variables["text_field"] = cmds.textField("text_select")
-		self.variables["input_text_button"] = cmds.button(label='Assign Surface', command = inputText)
-		self.variables["deselect_endJoints_button"] = cmds.button(label='Do Thing', command = deselectEndJoints)
+		self.variables["input_text_button"] = cmds.button(label='Assign Surface', command = self.inputText)
+		self.variables["deselect_endJoints_button"] = cmds.button(label='Do Thing', command = self.deselectEndJoints)
 		cmds.showWindow()
 	
 	#Select hierarchy and remove end joints
 	def deselectEndJoints(self, *args):
-		selection = cmds.ls(sl = 1)
-		cmds.select(selection, hierarchy = 1)
+		cmds.select(self.variables["current_selection"][0], hierarchy = 1)
 		full_selection = cmds.ls(sl = 1)
 		for i in full_selection:
 			if "_end" in i:
@@ -26,7 +26,7 @@ class deselectEndJointsClass(object):
 
 	#Input the name of the selected item into the text box
 	def inputText(self, *args):
-		self.variables["current_selection"] =  = cmds.ls(selection=True)
-		cmds.textField(self.variables["text_field"], edit=True, text=sel[0])
+		self.variables["current_selection"] = cmds.ls(selection=True)
+		cmds.textField(self.variables["text_field"], edit=True, text=self.variables["current_selection"][0])
 
 	
