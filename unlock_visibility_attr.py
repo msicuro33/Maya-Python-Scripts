fb57142aaaf4324193263be58ab562533e5deeb6
Msicuro <35858231+Msicuro@users.noreply.github.com>
2021-03-27 15:56:07 -0400

    Last edit before clearing the previous iteration

--- unlock_visibility_attr.py
+++ unlock_visibility_attr.py
@@ -1,33 +1,48 @@
 import maya.cmds as cmds
 
+current_selection = cmds.ls(selection=True)
+
+def function(current_selection):
+	for i in current_selection:
+		is_keyable = cmds.getAttr(i + ".visibility", k = 1)
+		is_unlocked = cmds.getAttr(i + ".visibility", se = 1)
+		on_off = cmds.getAttr(i + ".visibility")
+
+		if is_keyable == False:
+			cmds.setAttr(selection + ".visibility", k = 1)
+		else:
+			print("Attribute is already keyable")
+		if is_unlocked == False:
+			cmds.setAttr(selection + ".visibility", l = False)
+		else:
+			print("Attribute is already unlocked")
+		if on_off == False:
+			cmds.setAttr(selection + ".visibility", True)
+		else:
+			print("Attribute is already on")
+
 class unlockVisibility(object):
 	"""docstring for unlockVisibility"""
 	def __init__(self, *args):
 		self.variables = {}
-		self.createUI()
-		
-	#Create the UI window
-	def createUI(self, *args):
-		self.variables["window"] = cmds.window(title = "unlock_visibility_window")
-		self.variables["rowLayout"] = cmds.rowColumnLayout(numberOfRows = 1)
-		self.variables["layout_text"] = cmds.text(label='Selection')
-		self.variables["text_field"] = cmds.textField("text_select")
-		self.variables["make_keyable_button"] = cmds.button(label='Make Visibility Keyable', command = self.)
-		self.variables["unlock_attr_button"] = cmds.button(label='Unlock Attribute', command = self.)
-		self.variables["turn_visibility_attr_on"] = cmds.button(label='Turn On Attribute', command = self.)
-		cmds.showWindow()
-	
-	#Select hierarchy and remove end joints
-	def deselectEndJoints(self, *args):
-		cmds.select(self.variables["current_selection"][0], hierarchy = 1)
-		full_selection = cmds.ls(sl = 1)
-		for i in full_selection:
-			if "_end" in i:
-				cmds.select(i, deselect = 1)
-
-	#Input the name of the selected item into the text box
-	def inputText(self, *args):
 		self.variables["current_selection"] = cmds.ls(selection=True)
-		cmds.textField(self.variables["text_field"], edit=True, text=self.variables["current_selection"][0])
+
+	def check_attribute(self, *args):
+		for i in self.variables["current_selection"]:
+			self.variables["is_keyable"] = cmds.getAttr(i + ".visibility", k = 1)
+			self.variables["is_unlocked"] = cmds.getAttr(i + ".visibility", se = 1)
+			self.variables["on_off"] = cmds.getAttr(i + ".visibility")
+			if self.variables["is_keyable"] == False:
+				raise errpr
+			if self.variables["is_unlocked"] == True:
+				raise error
+			if self.variables["on_off"] == False:
+				raise error
+
+	def set_visibility_attribute(self, selection):
+		cmds.setAttr(selection + ".visibility", k = 1)
+		cmds.setAttr(selection + ".visibility", l = False)
+		cmds.setAttr(selection + ".visibility", True)
+
 
 	
