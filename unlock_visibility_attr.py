cff5ddb1230b57bb993618a64abcb5029e979ff1
Msicuro <35858231+Msicuro@users.noreply.github.com>
2022-05-26 22:49:49 -0400

    Random
    Haven't checked in a while so not sure what I did, looks minor tho

--- unlock_visibility_attr.py
+++ unlock_visibility_attr.py
@@ -1,12 +1,15 @@
 import maya.cmds as cmds
 
 def unlockVisibility():
+	#Stores the current selction(can be anything, mostly intended for joints)
 	current_selection = cmds.ls(selection=True)
+
+	#Queries if the Visibility attribute of the current selection is keyable, unlocked and/or already on
 	for i in current_selection:
 		is_keyable = cmds.getAttr(i + ".visibility", k = 1)
 		is_unlocked = cmds.getAttr(i + ".visibility", se = 1)
 		on_off = cmds.getAttr(i + ".visibility")
-
+		# Checks if the attribute is keyable, unlocked or on and outputs a message or turns on the attribute
 		if is_keyable == False:
 			cmds.setAttr(i + ".visibility", k = 1)
 		else:
